e90970b69ef137bd40e781e94dc5cbc3f86213e7
Marc Jessome <marc.jessome@intel.com>
2013-05-30 17:12:39 -0400

    Add Sema tests for simd linear clause.

--- test/Parser/pragma-simd.cpp
+++ test/Parser/pragma-simd.cpp
@@ -23,22 +23,9 @@ struct A {
 };
 
 
-void foo(int a[]) {
-  extern int b[];
-  #pragma simd linear(a)
-  for (int i = 0; i < 10; i++) ;
-
-  /* expected-error@+1 {{invalid linear variable}} */
-  #pragma simd linear(b)
-  for (int i = 0; i < 10; i++) ;
-}
-
-
 int z;
-const int C0 = 0;
 const int C1 = 1;
 const int C2 = 2;
-
 void test_linear()
 {
   int y;
@@ -62,35 +49,4 @@ void test_linear()
   for (int i = 0; i < 10; ++i) ;
   #pragma simd linear(y:c)
   for (int i = 0; i < 10; ++i) ;
-
-  /* expected-error@+1 {{invalid linear variable}} */
-  #pragma simd linear(a.a:1)
-  for (int i = 0; i < 10; ++i) ;
-  /* expected-error@+1 {{invalid linear step: expected integral constant or variable reference}} */
-  #pragma simd linear(y:y*z)
-  for (int i = 0; i < 10; ++i) ;
-  /* expected-error@+1 {{invalid linear step: expected integral constant or variable reference}} */
-  #pragma simd linear(y:a.a)
-  for (int i = 0; i < 10; ++i) ;
-
-  int ai[10];
-  /* expected-error@+1 {{invalid linear variable}} */
-  #pragma simd linear(ai)
-  for (int i = 0; i < 10; ++i);
-  /* expected-error@+1 {{invalid linear variable}} */
-  #pragma simd linear(ai[1])
-  for (int i = 0; i < 10; ++i);
-
-#pragma clang diagnostic push
-#pragma clang diagnostic warning "-Wsource-uses-cilk-plus"
-  /* expected-warning@+1 {{linear step is zero}} */
-  #pragma simd linear(y:0)
-  for (int i = 0; i < 10; ++i);
-  /* expected-warning@+1 {{linear step is zero}} */
-  #pragma simd linear(y:C0)
-  for (int i = 0; i < 10; ++i);
-  /* expected-warning@+1 {{linear step is zero}} */
-  #pragma simd linear(y:C1-C1)
-  for (int i = 0; i < 10; ++i);
-#pragma clang diagnostic pop
 }
--- test/Sema/pragma-simd.c
+++ test/Sema/pragma-simd.c
@@ -1,6 +1,7 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
 
 int I();
+const int C0 = 0;
 const int C1 = 1;
 const int C2 = 2;
 const char CC = (char)32;
@@ -29,4 +30,61 @@ void test_vectorlength() {
   #pragma simd vectorlength(C1+j)
   /* expected-error@+1 {{invalid vectorlength expression: must be an integer constant}} */
   #pragma simd vectorlength(I())
+}
+
+struct A { int a; };
+
+void test_linear(int arr[]) {
+  int i, j, k = 1;
+
+  struct A a;
+  a.a = 2;
+
+  #pragma simd linear(arr)
+  for (int i = 0; i < 10; i++) ;
+  #pragma simd linear(j)
+  for (i = 0; i < 10; ++i) ;
+  #pragma simd linear(j:1)
+  for (i = 0; i < 10; ++i) ;
+  #pragma simd linear(j:C1)
+  for (i = 0; i < 10; ++i) ;
+  #pragma simd linear(j:C1+C2)
+  for (i = 0; i < 10; ++i) ;
+  #pragma simd linear(j:k)
+  for (i = 0; i < 10; ++i) ;
+
+  extern int arr_b[];
+  /* expected-error@+1 {{invalid linear variable}} */
+  #pragma simd linear(arr_b)
+  for (int i = 0; i < 10; i++) ;
+  /* expected-error@+1 {{invalid linear variable}} */
+  #pragma simd linear(a.a:1)
+  for (int i = 0; i < 10; ++i) ;
+  /* expected-error@+1 {{invalid linear step: expected integral constant or variable reference}} */
+  #pragma simd linear(j:k*k)
+  for (int i = 0; i < 10; ++i) ;
+  /* expected-error@+1 {{invalid linear step: expected integral constant or variable reference}} */
+  #pragma simd linear(j:a.a)
+  for (int i = 0; i < 10; ++i) ;
+
+  int ai[10];
+  /* expected-error@+1 {{invalid linear variable}} */
+  #pragma simd linear(ai)
+  for (int i = 0; i < 10; ++i);
+  /* expected-error@+1 {{invalid linear variable}} */
+  #pragma simd linear(ai[1])
+  for (int i = 0; i < 10; ++i);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic warning "-Wsource-uses-cilk-plus"
+  /* expected-warning@+1 {{linear step is zero}} */
+  #pragma simd linear(j:0)
+  for (int i = 0; i < 10; ++i);
+  /* expected-warning@+1 {{linear step is zero}} */
+  #pragma simd linear(j:C0)
+  for (int i = 0; i < 10; ++i);
+  /* expected-warning@+1 {{linear step is zero}} */
+  #pragma simd linear(j:C1-C1)
+  for (int i = 0; i < 10; ++i);
+#pragma clang diagnostic pop
 }
